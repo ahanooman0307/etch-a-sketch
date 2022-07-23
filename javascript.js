2fe656667ef6817949fa9e9ca784f2b24624723a
Akshay <ahanooman0307@gmail.com>
2022-07-23 16:36:16 -0400

    added more ui. does not work when grid size changes

--- javascript.js
+++ javascript.js
@@ -3,18 +3,25 @@ const grid = document.querySelector(".grid");
 const eraser = document.querySelector(".eraser");
 const chosenColor = document.querySelector("#color");
 const chosenSize = document.querySelector("#range");
+const showSize = document.querySelector(".text");
+const clear = document.querySelector(".clear")
 //new variables
 let gridSize = 16;
 let color = "black";
 
 
 
+
+
 //function to create grid
 function createGrid(gridSize){
+    let num = 1;
     let area = gridSize * gridSize; //area variable is # of boxes
     for(let i = 0; i < area; i++){
         const box = document.createElement('div');
         box.classList.toggle('grid-item');
+        box.setAttribute('id', `${num}`);
+        num++;
         grid.appendChild(box);
     }
     grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)` //gridSize variable is length and height of grid
@@ -29,6 +36,7 @@ function colorGrid(color){
 
 
 
+
 createGrid(gridSize);
 
 
@@ -38,18 +46,31 @@ chosenColor.addEventListener('input', () =>{
 eraser.addEventListener('click', () =>{
     color = "white";
 });
-chosenSize.addEventListener('input', () =>{
-    // box.classList.toggle('grid-item');
-    // grid.removeChild('grid-item');
-    gridSize = chosenSize.value;
-    createGrid(gridSize);
-})
 
 const box = document.querySelectorAll('.grid-item');
 // I used the .forEach method to iterate through each box
+
+
+chosenSize.addEventListener('input', () =>{
+
+        
+        const box = document.querySelectorAll('.grid-item');
+        box.forEach(box => box.remove());
+        gridSize = chosenSize.value;
+        showSize.textContent = `${gridSize} x ${gridSize}`
+        createGrid(gridSize);
+    })
+
 box.forEach((m) => {
   // and for each box I add a 'click' listener
-  m.addEventListener('mousedown', () => {
+  m.addEventListener('click', () => {
     m.style.cssText = `background-color: ${color}`;
   });
+  
 });
+
+clear.addEventListener('click', () =>{
+    const box = document.querySelectorAll('.grid-item');
+    box.forEach(box => box.style.cssText = "background-color: white");
+  
+})
